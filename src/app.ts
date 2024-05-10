8c4c70ab77ca896cd12d27b14bc6ee567029b2cd
Dalfonzo <29dalfonzo@gmail.com>
2024-05-10 12:40:04 -0400

    Add cors package and update user routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
-// import userRoutes from "./routes/index";
+import cors from "cors";
 import usersRouter from "./routes/users";
 import tasksRouter from "./routes/tasks";
 
 export const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
