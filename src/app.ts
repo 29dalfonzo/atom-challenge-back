632148f4a73867171b492d35e60f1c410d853eb5
Dalfonzo <29dalfonzo@gmail.com>
2024-05-10 10:16:51 -0400

    Add users and tasks routes and methods

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 // import userRoutes from "./routes/index";
-import router from "./routes/index";
+import usersRouter from "./routes/users";
+import tasksRouter from "./routes/tasks";
 
 export const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(router); // Usa el router importado
+app.use(usersRouter); // Usa el router importado
+app.use(tasksRouter); // Usa el router importado
+
 
 
 module.exports = {app };
