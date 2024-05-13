0befd40a7fdd0380c7a45325cbafec92d5c7c0fa
Dalfonzo <29dalfonzo@gmail.com>
2024-05-13 18:33:25 -0400

    Refactor Firebase initialization in firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,9 @@
 import {initializeApp, applicationDefault} from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-// Initialize Firebase Admin
-const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
-  };
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp({
+    credential: applicationDefault(),
+});
 
 // Initialize Firestore
 const db = getFirestore();
